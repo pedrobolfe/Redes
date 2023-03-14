aa8a9ce8cc7e72f4ace304fb890952458b48748b
pedro <pedroschroeder06@gmail.com>
2023-03-14 14:41:30 -0300

    commit 02

--- server_threads/server.py
+++ server_threads/server.py
@@ -8,18 +8,20 @@ class Contador(Thread):
         self.segundos = segundos
         self.nome = nome
         
-
     def run(self):
         for i in range(self.n):
             print(i+1)
             sleep(self.segundos)
         print(f'Thread {self.nome} finalizada')
 
-Contador(5, 2, "cleitin").start()
+def main():
+    Contador(5, 2, "cleitin").start()
+    Contador(10, 2, "Siuu").start()
+    Contador(15, 1, "Magaiver").start()
 
-Contador(10, 2, "Siuu").start()
-Contador(15, 1, "Magaiver").start()
-
+if __name__ == "__main__":
+    main()
+    
 # print("Esperando threads terminarem")
 # sleep(15)
 # print("Fim")
